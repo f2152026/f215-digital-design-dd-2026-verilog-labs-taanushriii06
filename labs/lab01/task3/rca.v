da2a3061728531b7571577ce8b2b1e39f5d0fddf
taanushriii06 <f20240871@pilani.bits-pilani.ac.in>
2026-08-28 18:29:41 +0000

    lab01 task3

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -21,5 +21,16 @@ module rca(
   wire c1, c2, c3;
 
   // TODO: your four FA_Gate instances go here.
+assign sum[0] = a[0] ^ b[0] ^ cin;
+assign c1 = (a[0] & b[0]) | (a[0] & cin) | (b[0] & cin);
+
+assign sum[1] = a[1] ^ b[1] ^ c1;
+assign c2 = (a[1] & b[1]) | (a[1] & c1) | (b[1] & c1);
+
+assign sum[2] = a[2] ^ b[2] ^ c2;
+assign c3 = (a[2] & b[2]) | (a[2] & c2) | (b[2] & c2);
+
+assign sum[3] = a[3] ^ b[3] ^ c3;
+assign cout = (a[3] & b[3]) | (a[3] & c3) | (b[3] & c3);
 
 endmodule
