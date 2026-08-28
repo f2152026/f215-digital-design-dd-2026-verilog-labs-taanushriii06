c0cbdfde6150f20d91619aa27090fcfef2fc2de8
taanushriii06 <f20240871@pilani.bits-pilani.ac.in>
2026-08-28 10:45:49 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(5) (ps,  a,   b);
-  and #(5) (pc1, a,   b);
-  xor #(5) (sum, cin, ps);
-  and #(5) (pc2, cin, ps);
-  or  #(5) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
